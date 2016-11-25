1f6622916703bc972786c6107b57eba9fb6ab97f
Yuting Chou <ytingchou@gmail.com>
2016-11-26 01:23:56 +1100

    Styled searchbar and added animation.

---
+++ Project3-Youtube-Search-Engine/searchvidz/js/script.js
@@ -0,0 +1,27 @@
+// Searchbar Handler
+$(function() {
+  var searchField = $('#query');
+  var icon = $('#search-btn');
+
+  // Focus Handler
+  $(searchField).on('focus', function() {
+    $(this).animate({
+      width: '100%'
+    }, 400);
+    $(icon).animate({
+      right: '10px'
+    }, 400);
+  });
+
+  // Blur Event Handler
+  $(searchField).on('blur', function() {
+    if (searchField.val() == '') {
+      $(searchField).animate({
+        width: '45%'
+      }, 400, function() {});
+      $(icon).animate({
+        right: '360px'
+      }, 400, function() {});
+    }
+  });
+});
