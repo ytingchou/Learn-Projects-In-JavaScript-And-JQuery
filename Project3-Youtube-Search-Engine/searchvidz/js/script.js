f63c5150f489e07a7a2c2744d402d94871afa82b
Yuting Chou <ytingchou@gmail.com>
2016-11-26 12:39:41 +1100

    Built results from search list

--- Project3-Youtube-Search-Engine/searchvidz/js/script.js
+++ Project3-Youtube-Search-Engine/searchvidz/js/script.js
@@ -56,3 +56,29 @@ function search() {
 
   return false;
 }
+
+// Build Output
+function getOutput(item) {
+  var videoId = item.id.videoId;
+  var title = item.snippet.title;
+  var description = item.snippet.description;
+  var thumb = item.snippet.thumbnails.high.url;
+  var channelTitle = item.snippet.channelTitle;
+  var videoDate = item.snippet.publishedAt;
+
+  // Build Output String;
+  var output = '<li>' +
+    '<div class="list-left">' +
+    '<img src="' + thumb + '">' +
+    '</div>' +
+    '<div class="list-right">' +
+    '<h3>' + title + '</h3>' +
+    '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>'+
+    '<p>' + description + '</p>' +
+    '</div>' +
+    '</li>' +
+    '<div class="clearfix"></div>' +
+    '';
+
+    return output;
+}
